404591b803d7b08a6173e5d6bdb1fc0cadd4b696
Your New Name <riteshpandey7356@gmail.com>
2024-03-27 12:33:00 +0530

    update

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://madextube.com/api/v1",
+  baseURL: "https://madextube700.com/api/v1",
   // baseURL: "http://localhost:8000/api/v1",
 });
 
